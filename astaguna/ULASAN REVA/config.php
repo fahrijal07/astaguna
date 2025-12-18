a85999ab39e14949176e50848d590679043d6d84
Fahri <fahrijal3007@gmail.com>
2025-12-18 11:32:52 +0700

    Delete astaguna directory

--- astaguna/ULASAN REVA/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "db_catering_jawa");
-
-if (!$koneksi) {
-    die("Koneksi gagal!");
-}
-?>
--- astaguna/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "astaguna");
-
-if (!$koneksi) {
-    die("Koneksi gagal!");
-}
-?>
